3755e73deea61b7c0b857113a775ff6bc1aa123c
hong-jen kao <endless-dreaming@hotmail.com>
2011-12-30 23:02:55 +0800

    看診處更新

--- TreatmentPrototypeSystem/src/InspectionRoom.java
+++ TreatmentPrototypeSystem/src/InspectionRoom.java
@@ -1,5 +1,9 @@
 
 public class InspectionRoom {
+    final int XRay = 0;
+    final int bloodPressure = 1;
+    final int sight = 2;
+    
     private String division;
     private InspectionList inspectionList;
 }
