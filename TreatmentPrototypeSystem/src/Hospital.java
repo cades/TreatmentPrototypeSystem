f46b98f770691b90579528939d9d68a578f4a59d
hong-jen kao <endless-dreaming@hotmail.com>
2011-12-30 13:48:17 +0800

    Add class 'Hospital' 醫院類別中有5個Vector，裝載5個職業。使用Vector是代表一對多的關係。 也提供建構子初始化它們。
    目前先把main放在這裡，未來會不會移出不確定。

---
+++ TreatmentPrototypeSystem/src/Hospital.java
@@ -0,0 +1,21 @@
+import java.util.Vector;
+
+public class Hospital {
+    Vector<Doctor> doctorVec;
+    Vector<Nurse> nurseVec;
+    Vector<Inspector> inspectorVec;
+    Vector<Pharmacist> pharmacistVec;
+    Vector<CounterStaff> counterStaffVec;
+    
+    public Hospital() {
+        doctorVec = new Vector<Doctor>();
+        nurseVec = new Vector<Nurse>();
+        inspectorVec = new Vector<Inspector>();
+        pharmacistVec = new Vector<Pharmacist>();
+        counterStaffVec = new Vector<CounterStaff>();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
