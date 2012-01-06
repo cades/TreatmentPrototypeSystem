b395b985b3b400b449a39bc16aea3ad0f20489f9
hong-jen kao <endless-dreaming@hotmail.com>
2012-01-06 23:21:12 +0800

    加上"是否印過"的flag 考慮到一個病人有很多藥單，護士應該只想看到最新的那份 所以加上isPrinted，只要印過就設成true，之後再印將不顯示這筆記錄。

--- TreatmentPrototypeSystem/src/storage/prescription/Prescription.java
+++ TreatmentPrototypeSystem/src/storage/prescription/Prescription.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class Prescription {
     String counterStaffInChargeId;
     String doctorInChargeId;
+    boolean isPrinted;
     boolean isMoneyPaid;
     boolean isMedicineReceived;
     Date time;
