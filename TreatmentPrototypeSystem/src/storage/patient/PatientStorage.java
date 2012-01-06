78030e2b8a488e292756d88e1e1f96a74eaa588d
hong-jen kao <endless-dreaming@hotmail.com>
2012-01-06 15:29:58 +0800

    修正bug

--- TreatmentPrototypeSystem/src/storage/patient/PatientStorage.java
+++ TreatmentPrototypeSystem/src/storage/patient/PatientStorage.java
@@ -7,6 +7,11 @@ import java.util.*;
  * 存病人資料的資料庫
  */
 public class PatientStorage extends HashMap<String, Patient> {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
     
 //    public boolean validate(String id, String password) {
 //        if (this.containsKey(id) && this.get(id).password().equals(password)) return true;
