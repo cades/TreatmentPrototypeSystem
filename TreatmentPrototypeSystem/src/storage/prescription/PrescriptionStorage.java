f4595b4c17cfbb5a81f37daf675f6d24b56b21ce
hong-jen kao <endless-dreaming@hotmail.com>
2012-01-06 23:23:26 +0800

    將PrescriptionStorage做成singleton

--- TreatmentPrototypeSystem/src/storage/prescription/PrescriptionStorage.java
+++ TreatmentPrototypeSystem/src/storage/prescription/PrescriptionStorage.java
@@ -5,8 +5,16 @@ import java.util.ArrayList;
 public class PrescriptionStorage extends ArrayList<Prescription>{
 
     /**
-     * 
+     * PrescriptionStorage applies singleton pattern
      */
     private static final long serialVersionUID = 1L;
-
+    
+    private static PrescriptionStorage _instance = null;
+    protected PrescriptionStorage() { super(); }
+    public PrescriptionStorage Instance() {
+        if (_instance == null) {
+            _instance = new PrescriptionStorage();
+        }
+        return _instance;
+    }
 }
